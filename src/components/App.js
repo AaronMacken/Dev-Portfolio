aada3c673ffc58fd8199d8598d470e4dee66e786
Aaron Macken <aaron.a.macken@gmail.com>
2019-10-19 09:23:46 -0700

    Add css file import statement in app.css

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Footer from "./Re-usable/Footer/Footer";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
+import "./App.css";
+
 class App extends Component {
   render() {
     return (
