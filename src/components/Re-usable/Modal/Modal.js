70ebc058d2adbf443bbceb6242cd0df3e1a1f58a
Aaron Macken <aaron.a.macken@gmail.com>
2019-11-08 16:38:15 -0800

    First version of info modal for project page

--- src/components/Re-usable/Modal/Modal.js
+++ src/components/Re-usable/Modal/Modal.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
-import './Modal.css';
+import React, { Component } from "react";
+import "./Modal.css";
 
 class Modal extends Component {
+  render() {
+    const { show, close, modalData } = this.props;
+    const modalWrapperStyle = {
+      visibility: show ? "visible" : "hidden",
+      transform: show ? "translate(-50%, -45%)" : "translate(-50%, -100vh)",
+      opacity: show ? "1" : "0"
+    };
 
-    render() {
-        const {show, close, modalData} = this.props;
-        const modalWrapperStyle = {
-            visibility: show ? 'visible' : 'hidden',
-            transform: show ? 'translate(-50%, -45%)' : 'translate(-50%, -100vh)',
-            opacity: show ? '1' : '0'
-        }
+    const myModalBullets = modalData.modalBullets.map(e => {
+      return <li key={e}>{e}</li>;
+    });
 
-        const myModalBullets = modalData.modalBullets.map((e) => {
-            return (
-                <li key={e}>{e}</li>
-            )
-        })
+    return (
+      <div>
+        <div style={modalWrapperStyle} className="modal-wrapper">
+          <div className="modal-header">
+            <h3>{modalData.modalTitle}</h3>
+            <span className="close-modal-btn" onClick={close}>
+              x
+            </span>
+          </div>
+          <div className="modal-body">
+            <p className="modal-description">{modalData.modalDescription}</p>
+            <ul className="modal-list">{myModalBullets}</ul>
+          </div>
 
-        return (
-            <div>
-                <div style={modalWrapperStyle} className="modal-wrapper">
-
-                    <div className="modal-header">
-                        <h3>{modalData.modalTitle}</h3>
-                        <span className="close-modal-btn" onClick={close}>x</span>
-                    </div>
-                    <p>{modalData.modalDescription}</p>
-                    <ul>
-                        {myModalBullets}
-                    </ul>
-                    <div className="modal-footer">
-                        <button className="btn-cancel" onClick={close}>CLOSE</button>
-                        <button form="newSongForm" type="submit" className="btn-continue" >Submit</button>
-                    </div>
-
-
-                </div>
-            </div>
-        )
-    }
+          <div className="modal-footer">
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Modal;
