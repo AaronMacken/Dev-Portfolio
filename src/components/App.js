5144a1c42a4d74b676af48dcd811699f723c1d08
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-21 15:28:55 -0800

    Add https redirect

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./Re-usable/Navbar/Navbar";
 import Main from "./Main/Main";
 import Footer from "./Re-usable/Footer/Footer";
+import HttpsRedirect from 'react-https-redirect';
 
 import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./Re-usable/ScrollToTop/ScrollToTop";
@@ -11,14 +12,17 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <ScrollToTop />
-        <div className="App">
-          <Navbar />
-          <Main />
-          <Footer />
-        </div>
-      </Router>
+      <HttpsRedirect>
+        <Router>
+          <ScrollToTop />
+          <div className="App">
+            <Navbar />
+            <Main />
+            <Footer />
+          </div>
+        </Router>
+      </HttpsRedirect>
+
     );
   }
 }
